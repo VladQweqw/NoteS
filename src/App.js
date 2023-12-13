d60e3db8633df80a5f976968d2c802313707af6b
qweqw <81089518+VladQweqw@users.noreply.github.com>
2023-12-13 23:16:52 +0200

    added lots

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useMemo, useState, useEffect } from 'react';
+import { forwardRef , useState, useEffect, useRef } from 'react';
 
 // Syles
 import './static/style.css'
@@ -6,12 +6,57 @@ import './static/style.css'
 const fs = window.require('fs')
 const pathModule = window.require('path')
 
+const initialNotes = [
+  {
+    title: 'Sandu',
+    text: 'AWD',
+  },
+  {
+    title: 'Ionut',
+    text: 'sugi pula ionut',
+  },
+  {
+    title: 'Danutu',
+    text: 'Ba <i>CAL<i> in el lmc <c class="accent">ma</c> rupe maine',
+  },
+  {
+    title: 'Bodorineeee',
+    text: 'sugi pula ionut',
+  },
+]
+
 function App() {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState(initialNotes)
   const [currentIndex, setCurrentIndex] = useState(0)
+  const textArea = useRef(null)
+
+  function searchNotes(search) {
+    if(search.trim() === '') return setNotes(initialNotes)
+    const keyword = search.toLowerCase()
+    const notesCopy = initialNotes
+
+    const filteredNotes = notesCopy.filter((note) => {
+      const noteTitle = note.title.toLocaleLowerCase()
+
+      return noteTitle.indexOf(keyword) > -1
+    })
+
+    setNotes(filteredNotes)
+  }
   
+
   useEffect(() => {
-    console.log(currentIndex);
+    if(textArea.current)
+      textArea.current.innerHTML = notes[currentIndex].text
+
+      const notes_elem = document.querySelectorAll('.note')
+
+      notes_elem.forEach((note) => {
+        note.classList.remove('note-active')
+        notes_elem[currentIndex].classList.add('note-active')
+      })
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentIndex])
   
 
@@ -21,7 +66,9 @@ function App() {
       <div className="sidebar">
         <div className="top-sidebar">
           <div className="search-wrapper">
-            <input type="search" name="Search notes" id="search-field" placeholder='Search...' />
+            <input onChange={(e) => {
+              searchNotes(e.target.value)
+            }} type="search" name="Search notes" id="search-field" placeholder='Search...' />
           </div>
 
           <div className="sidebar-options">
@@ -77,7 +124,7 @@ function App() {
 
       <article className="note-content-wrapper"> 
           {notes[currentIndex]?.text ? 
-            <textarea name="" id="note-content"></textarea>
+            <NoteContent ref={textArea} note={notes[currentIndex]} />
           : <p className='no-data-text'>Nothing here yet, click on a note or create one to start</p>  
         }
       </article> 
@@ -86,11 +133,85 @@ function App() {
   );
 }
 
-function Note(data) {
 
+const NoteContent = forwardRef(function(props, ref) {
+  const [text, setText] = useState(props.note.title)
+  const [currentColor, setCurrentColor] = useState('#fff')
+
+  useEffect(() => {
+    setText(props.note.title)
+  }, [props.note])
+  
+  const handleChange = event => {
+    setText(event.target.value);
+  };
+
+  return <>
+    <header className="note-content-header">
+      <div className="title-wrapper">
+        <input 
+        className="note-title" 
+        value={text}
+        onChange={handleChange}
+        onBlur={() => {
+        // saveTitle()
+      }}
+      ></input>
+        <svg xmlns="http://www.w3.org/2000/svg" className='svg' viewBox="0 0 23 23" fill="none">
+        <g clipPath="url(#clip0_6_195)">
+          <path d="M7.66671 19.1667L17.7292 9.10417C17.9809 8.85247 18.1806 8.55366 18.3168 8.2248C18.453 7.89593 18.5231 7.54346 18.5231 7.1875C18.5231 6.83155 18.453 6.47907 18.3168 6.15021C18.1806 5.82135 17.9809 5.52254 17.7292 5.27084C17.4775 5.01914 17.1787 4.81948 16.8498 4.68326C16.521 4.54704 16.1685 4.47693 15.8125 4.47693C15.4566 4.47693 15.1041 4.54704 14.7752 4.68326C14.4464 4.81948 14.1476 5.01914 13.8959 5.27084L3.83337 15.3333V19.1667H7.66671Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M12.9375 6.22919L16.7708 10.0625" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M15.3334 17.25H19.1667" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+        </g>
+        <defs>
+          <clipPath id="clip0_6_195">
+            <rect width="23" height="23" fill="white"/>
+          </clipPath>
+        </defs>
+        </svg>
+      </div>
+
+      <div className="header-options">
+          <input onBlur={() => {
+            let tr = window.getSelection().getRangeAt(0);
+            tr.innerHTML = ``
+            let span = document.createElement("span");
+            span.style.color = currentColor;
+            span.appendChild(tr.extractContents());
+            tr.insertNode(span)
+          
+            
+
+          }} type="color" className='input color-input' id="current-color" onChange={(e) => {
+            setCurrentColor(e.target.value)
+          }} name="Text color" />
+
+          <svg xmlns="http://www.w3.org/2000/svg" className='svg' viewBox="0 0 30 30" fill="none">
+            <g clipPath="url(#clip0_29_66)">
+              <path d="M5 10V7.5C5 6.83696 5.26339 6.20107 5.73223 5.73223C6.20107 5.26339 6.83696 5 7.5 5H10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M5 20V22.5C5 23.163 5.26339 23.7989 5.73223 24.2678C6.20107 24.7366 6.83696 25 7.5 25H10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M20 5H22.5C23.163 5 23.7989 5.26339 24.2678 5.73223C24.7366 6.20107 25 6.83696 25 7.5V10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M20 25H22.5C23.163 25 23.7989 24.7366 24.2678 24.2678C24.7366 23.7989 25 23.163 25 22.5V20" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M15 20V11.25" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M11.25 11.25H18.75" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </g>
+            <defs>
+    <clipPath id="clip0_29_66">
+      <rect width="30" height="30" fill="white"/>
+    </clipPath>
+  </defs>
+          </svg>
+      </div>
+    </header>
+    <pre id="note-content" ref={ref} contentEditable></pre>
+  </>
+})
+
+function Note(data) {
   return(
-    <div onClick={() => {
+    <div onClick={(e) => {
       data.setCurrentIndex(data.index)
+      
     }} className={`note ${data.class}`}>
       <p className='note-name'>{data.note.title}</p>
     </div>
